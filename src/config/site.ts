74dfd5589b3c4a8fedac2434f15e3eac94e8f587
vijaymanu969 <vijaymanu969@gmail.com>
2024-07-30 12:08:44 +0530

    feat: update contact form and site configuration
    - Ensure contact form captures emails correctly.
    - Update site configuration for better SEO.

--- src/config/site.ts
+++ src/config/site.ts
@@ -5,8 +5,8 @@ const siteConfig = {
     "LOKKEE STUDIOS is bringing you web development and design from the future. Our expertise is developing next-level websites and web applications including full frontend design. Why don't you check out the fancy time machine we built over there?",
   backgroundColor: '#060918',
   themeColor: '#6919FF',
-  email: 'hello@lokkeestudios.com',
-  url: 'https://lokkeestudios.com',
+  email: 'office@celumestudios.com',
+  url: 'https://celumestudios.com',
   links: {
     sourcecode: 'https://sourcecode.lokkeestudios.com',
     github: 'https://github.lokkeestudios.com',
